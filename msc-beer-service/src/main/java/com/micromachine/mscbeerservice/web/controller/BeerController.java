f88cb730ef6714cb0a8da7d3ceca94a2f7b23736
atlroc99 <jeffry.zaman@gmail.com>
2020-07-25 13:11:13 -0400

    added beer controller

---
+++ msc-beer-service/src/main/java/com/micromachine/mscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,41 @@
+package com.micromachine.mscbeerservice.web.controller;
+
+import com.micromachine.mscbeerservice.web.model.BeerDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @Value("${hostAndPort}")
+    private String hostAndPort;
+
+    @Value("${beer.apiPath}")
+    private String beerApipath;
+
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId){
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity addBeer(@RequestBody BeerDto beerDto){
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeer(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beerId}")
+    public ResponseEntity deleteBeer(@PathVariable UUID beerId) {
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
