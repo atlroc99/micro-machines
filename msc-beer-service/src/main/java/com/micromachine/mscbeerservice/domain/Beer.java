71272b7b56b91ca7713aa10a5df3eeb3b80a8c60
atlroc99 <jeffry.zaman@gmail.com>
2020-07-25 17:54:33 -0400

    added beer domain and repository

--- msc-beer-service/src/main/java/com/micromachine/mscbeerservice/domain/Beer.java
+++ msc-beer-service/src/main/java/com/micromachine/mscbeerservice/domain/Beer.java
@@ -1,4 +1,43 @@
 package com.micromachine.mscbeerservice.domain;
 
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+@Entity
 public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar(36)", nullable = false, updatable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    private String beerName;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private String upc;
+
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
 }
